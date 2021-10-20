79fa108eecb0fee278ec0feabe3223de5fd7a431
EverStarck <EverStarckOne@protonmail.com>
2021-10-20 00:54:18 -0500

    🔥🔥

--- igfollowing.py
+++ igfollowing.py
@@ -4,8 +4,8 @@ import ast
 import sys, colorama
 from colorama import Fore, Style
 
+COOKIE = '' # <--- HERE
 USER_AGENT = "Mozilla/5.0 (iPhone; CPU iPhone OS 12_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Instagram 105.0.0.11.118 (iPhone11,8; iOS 12_3_1; en_US; en-US; scale=2.00; 828x1792; 165586599)"
-COOKIE = ''
 headers = {"user-agent": USER_AGENT, "cookie": COOKIE}
 
 
@@ -17,6 +17,14 @@ try:
     saveFollows = int(sys.argv[2])
 except:
     pass
+
+
+#initialize colorama
+colorama.init()
+bad = Fore.RED + '[-]' + Style.RESET_ALL
+good = Fore.GREEN + '[+]' + Style.RESET_ALL
+alert = Fore.YELLOW + '[Δ]' + Style.RESET_ALL
+
 
 # Get user ID with profile name
 def getID():
@@ -31,7 +39,7 @@ def getID():
         }
     except:
         print(
-            f"Hubo un error obteniendo el usuario {profile}\nSi el user está bien entonces es un error de cookies"
+            f"There was an error getting the user {profile}\nIf the user is ok then it is a cookie error"
         )
         sys.exit()
 
@@ -43,10 +51,10 @@ def saveFile(itemToWrite):
         # Create file
         with open(f"./{titleFile}.txt", "w") as file:
             file.write(str(itemToWrite))
-        print("Usuarios seguidos guardados con exito")
+        print("Followed users saved successfully")
         return True
     except:
-        print("Hubo un error guardando los usuarios en el archivo")
+        print("There was an error saving users into file")
         return False
 
 
@@ -59,17 +67,45 @@ def compareFollows(freshList):
             txtSet = set()
             for user in txtList:
                 txtSet.add(user["id"])
-            print(txtSet)
             freshSet = set()
             for user in freshList:
                 freshSet.add(user["id"])
-            print(freshSet)
 
-            print(txtSet - freshSet, "A-B")
+            justB = list(txtSet - freshSet)  # A-B (Actualmente ya no lo sigue, por lo que en freshList no está pero en txt si)
+            justA = list(freshSet - txtSet)  # B-A (Lo sigue en ig pero no está en el txt)
 
+            if len(justB) == 0 and len(justA) == 0:
+                print("There were no changes")
+                return
+
+            changesB = []
+            changesA = []
+            # A-B
+            for id in justB:
+                for user in txtList:
+                    if id == user["id"]:
+                        changesB.append(user)
+            # B-A
+            for id in justA:
+                for user in freshList:
+                    if id == user["id"]:
+                        changesA.append(user)
+
+            if len(changesB) == 0:
+                print("There are no accounts that you used to follow but no longer\n")
+            else:
+                print("Accounts that you no longer follow but that you followed before:")
+                for acc in changesB:
+                    print(f"{bad} {acc['id']} ---- {acc['username']}\n")
+            if len(changesA) == 0:
+                print("There aren't new follows\n")
+            else:
+                print("Tracked accounts that are not in your txt:")
+                for acc in changesA:
+                    print(f"{good} {acc['id']} ---- {acc['username']}\n")
         return True
     except:
-        print("Hubo un error intentando abrir el archivo")
+        print("There was an error trying to open the file. Check that everything is fine and if the error persists, generate a new one")
         return False
 
 
@@ -94,11 +130,22 @@ def getFollowers():
             compareFollows(followingList)
             return
         else:
-            print("Hubo un error obteniendo los followers")
+            print("There was an error getting the followers")
             return False
     except:
-        print("Hubo un error obteniendo los followers\nQuizas es un error de cookies")
+        print("There was an error getting the followers\nMaybe it's a cookie error")
         sys.exit()
 
 
+print("""
+    _       ______      ____              _            
+   (_)___ _/ ____/___  / / /___ _      __(_)___  ____ _
+  / / __ `/ /_  / __ \/ / / __ \ | /| / / / __ \/ __ `/
+ / / /_/ / __/ / /_/ / / / /_/ / |/ |/ / / / / / /_/ / 
+/_/\__, /_/    \____/_/_/\____/|__/|__/_/_/ /_/\__, /  
+  /____/                                      /____/   
+ \n""")
+
 getFollowers()
+
+print(f"\n{alert}{alert} No changes were made to your txt. If you want to update it, you must execute the command again: python igFollowing.py acountName 1")
